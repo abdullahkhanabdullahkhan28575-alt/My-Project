3f5a723d8845ae96d0a6bb9cff89c39eee06be0c
Abdullah Khan <abdullahkhanabdullahkhan28575@gmail.com>
2026-05-25 23:44:00 +0530

    Add initial TO - DO List project

---
+++ script.js
@@ -0,0 +1,28 @@
+function addTask() {
+
+    let input = document.getElementById("taskInput");
+
+    let task = input.value;
+
+    if(task === ""){
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    li.innerHTML = `
+        ${task}
+        <button class="delete-btn" onclick="deleteTask(this)">
+            Delete
+        </button>
+    `;
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
+
+function deleteTask(button){
+    button.parentElement.remove();
+}
